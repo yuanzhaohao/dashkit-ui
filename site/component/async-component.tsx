2b0b7c299faf9db852b25e45e1e95e2e76430542
yuanzhaohao <yuanzhaohao@163.com>
2018-08-16 12:08:16 +0800

    feat: button component

--- site/component/async-component.tsx
+++ site/component/async-component.tsx
@@ -2,29 +2,27 @@ import * as React from 'react';
 import { Spin } from '../../src';
 
 type State = {
-  component: any,
+  component: React.ComponentType | null;
 };
 export default function asyncComponent(importComponent: any) {
-  class AsyncComponent extends React.Component<State> {
-    constructor(props: any) {
-      super(props)
-      this.state = {
-        component: null
-      }
-    }
+  class AsyncComponent extends React.Component<any, State> {
+   public state = {
+      component: null
+    };
 
     public async componentDidMount() {
       const { default: component } = await importComponent();
 
       this.setState({
-        component
+        component,
       });
     }
 
     public render() {
+      const { component } = this.state;
       return (
-        this.state.component
-          ? <this.state.component />
+        component
+          ? <component />
           : <div className="page-loading">
             <Spin text="Loading..." />
           </div>
