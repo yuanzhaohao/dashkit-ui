6432cb571dfc5d357b9d00619dfbda471c13ff32
yuanzhaohao <yuanzhaohao@163.com>
2018-08-17 11:53:15 +0800

    feat: card component

---
+++ site/component/common/example.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './example.scss';
+
+class Example extends React.PureComponent {
+  render() {
+    return (
+      <div className="app-example"></div>
+    )
+  }
+}
+
+export default Example;
