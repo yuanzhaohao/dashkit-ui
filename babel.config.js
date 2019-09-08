c56f966c22cd17d384745cfdf174f3564112a5aa
yuanzhaohao <yuanzhaohao@163.com>
2019-09-08 18:27:21 +0800

    ! feat: emitDeclarationOnly when publish

--- babel.config.js
+++ babel.config.js
@@ -4,38 +4,39 @@ module.exports = function(api) {
 
   api && api.cache(false);
 
+  const presets = [
+    [
+      '@babel/preset-env',
+      {
+        loose: true,
+        modules: useESModules ? false : 'commonjs',
+      },
+    ],
+    [
+      '@babel/preset-react',
+      {
+        development: NODE_ENV === 'development',
+      },
+    ],
+    '@babel/preset-typescript',
+  ];
+  const plugins = [
+    '@babel/plugin-proposal-class-properties',
+    [
+      '@babel/plugin-transform-runtime',
+      {
+        corejs: false,
+        helpers: true,
+        regenerator: false,
+        useESModules,
+      },
+    ],
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-transform-object-assign',
+  ];
+
   return {
-    presets: [
-      [
-        '@babel/preset-env',
-        {
-          loose: true,
-          modules: useESModules ? false : 'commonjs',
-        },
-      ],
-      [
-        '@babel/preset-react',
-        {
-          development: NODE_ENV === 'development',
-        },
-      ],
-      '@babel/preset-typescript',
-    ],
-    plugins: [
-      '@babel/plugin-proposal-class-properties',
-      [
-        '@babel/plugin-transform-runtime',
-        {
-          corejs: false,
-          helpers: true,
-          regenerator: false,
-          useESModules,
-        },
-      ],
-      '@babel/plugin-syntax-dynamic-import',
-      '@babel/plugin-transform-object-assign',
-    ],
+    presets,
+    plugins,
   };
 };
-const { BABEL_MODULE, NODE_ENV } = process.env;
-const useESModules = BABEL_MODULE !== 'commonjs' && NODE_ENV !== 'test';
