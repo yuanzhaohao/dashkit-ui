aa6c0b1a55277bc5ac1d1fe44f5a769981db5f1c
yuanzhaohao <yuanzhaohao@163.com>
2019-09-16 21:26:05 +0800

    ! feat: update Form test

--- src/form/__test__/index.test.tsx
+++ src/form/__test__/index.test.tsx
@@ -1,6 +1,4 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import * as TestUtils from 'react-dom/test-utils';
 import * as enzyme from 'enzyme';
 import Form from '..';
 import Input from '../../input';
