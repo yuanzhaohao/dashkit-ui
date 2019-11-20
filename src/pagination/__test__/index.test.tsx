3216070f7d432fe56d7e8b84afac3e8fb412bb05
yuanzhaohao <yuanzhaohao@163.com>
2019-11-20 16:34:52 +0800

    ! feat: pagination test

--- src/pagination/__test__/index.test.tsx
+++ src/pagination/__test__/index.test.tsx
@@ -10,8 +10,41 @@ describe('Pagination[Basic]', () => {
     wrapper
       .find(`.${prefixCls}-item`)
       .first()
-      .prop('onClick')(0);
+      .simulate('click');
 
     expect(changeMock.mock.calls[0][0]).toBe(1);
+  });
+
+  test('should disabled items', () => {
+    const wrapper = enzyme.mount(<Pagination current={1} total={2} disabled />);
+    wrapper.find(`${prefixCls}-item`).forEach(item => {
+      expect(item.prop('disabled')).toBeTruthy();
+    });
+  });
+
+  test('should minus while prev click', () => {
+    const wrapper = enzyme.mount(<Pagination current={10} total={1000} />);
+    wrapper
+      .find(`.${prefixCls}-prev`)
+      .first()
+      .simulate('click');
+
+    wrapper.update();
+
+    setTimeout(() => {
+      expect(wrapper.state('current')).toBe(9);
+    }, 0);
+  });
+
+  test('should add while next click', () => {
+    const wrapper = enzyme.mount(<Pagination current={10} total={1000} />);
+    wrapper
+      .find(`.${prefixCls}-next`)
+      .first()
+      .simulate('click');
+
+    setTimeout(() => {
+      expect(wrapper.state('current')).toBe(11);
+    }, 0);
   });
 });
